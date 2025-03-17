23c32239d73922fc3a093b3b1caae772b9359b10
Murasulimaran Kathiravan <maran.mk10298@gmail.com>
2025-03-17 22:24:01 +0530

    Add files via upload

---
+++ medical-project/javaScript.js
@@ -0,0 +1,21 @@
+
+ScrollReveal({ 
+    reset: true,
+    distance: '100px',
+    duration: 2000,
+    delay: 200,
+
+});
+
+ScrollReveal().reveal('.ma-cont1-subHead', { origin: 'top' });
+
+ScrollReveal({ 
+    reset: true,
+    distance: '100px',
+    duration: 2000,
+    delay: 200,
+
+});
+
+ScrollReveal().reveal('.ma-cont2, .ma-cont3, .awards, .ma-cont4, .cont5, .ma-cont6, .ma-cont7', 
+{ origin: 'bottom' });
